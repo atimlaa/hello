b10c3f1e475dd2749b81439498a4c5dc665c157a
atimlaa <54657323+atimlaa@users.noreply.github.com>
2019-08-29 14:16:14 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Aryan Timla and Tiffany Domingo
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Aryan Timla and Tiffany Domingo\n");
+
+  return 0;
+}
